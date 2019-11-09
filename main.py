d4a8c35210f74da55a6d79fd021bbe51b88acfb4
Branch321 <tythatsk8s05@yahoo.com>
2019-11-08 21:07:34 -0600

    Fixed modifiers.

--- main.py
+++ main.py
@@ -14,15 +14,13 @@ def choose_skills_unit_test(test_number):
         sum = 0
         dictionary = choose_skills()
         if "Power * (Brain / Brawn)" in dictionary.keys():
-            print(dictionary)
+            # print(dictionary)
             for element in dictionary.keys():
                 if element == "Power * (Brain / Brawn)":
                     sum+=dictionary[element]*2
                 else:
                     sum += dictionary[element]
-            if sum == 9:
-                print("Passing")
-            else:
+            if sum != 9:
                 print("Failed")
 
 def choose_skills():
@@ -39,12 +37,16 @@ def choose_skills():
         how_much_skill = 0
         if which_skill == 11 and total_skills_points>1:
             how_much_skill = random.randint(1, total_skills_points//2)
+            if list_o_skills[which_skill] not in character_skill_list.keys():
+                character_skill_list[list_o_skills[which_skill]] = 0
             total_skills_points -= how_much_skill*2
-            character_skill_list[list_o_skills[which_skill]] = how_much_skill
-        elif which_skill!=11:
+            character_skill_list[list_o_skills[which_skill]] += how_much_skill
+        elif which_skill != 11 and total_skills_points >= 1:
             how_much_skill = random.randint(1,total_skills_points)
+            if list_o_skills[which_skill] not in character_skill_list.keys():
+                character_skill_list[list_o_skills[which_skill]] = 0
             total_skills_points -= how_much_skill
-            character_skill_list[list_o_skills[which_skill]] = how_much_skill
+            character_skill_list[list_o_skills[which_skill]] += how_much_skill
     return character_skill_list
 
 character_sheet = []
@@ -55,10 +57,13 @@ number = random.randint(5,15)
 character_sheet.append('Number: ' + str(number) + ' ' + ("(Brain)\n" if number <= 10  else "(Brawn)\n"))
 character_sheet.append("Armor: " + str(20-number) + "\n")
 character_sheet.append('Health Points: 3 \n')
+character_sheet.append('Weapon: \n')
+character_sheet.append('Items: \n')
 character_sheet.append('Skills: \n')
 skill_dict = choose_skills()
 for element in skill_dict.keys():
-    character_sheet.append('    ' + str(element) + ': ' + str(skill_dict[element]) + '\n')
+    character_sheet.append(
+        '    ' + str(element) + ': ' + ('-' if "Brawn" in str(element) else '+') + str(skill_dict[element]) + '\n')
 character_sheet.append('\n')
 character_sheet.append('Notes: \n')
 print(*character_sheet)
